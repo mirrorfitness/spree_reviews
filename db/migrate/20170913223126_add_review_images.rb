44d40c0fcf580d75462f1dea98e519264ab6031d
Asher Van Brunt <asher@okbreathe.com>
2017-09-13 19:33:08 -0700

    Change table name

--- db/migrate/20170913223126_add_review_images.rb
+++ db/migrate/20170913223126_add_review_images.rb
@@ -1,6 +1,6 @@
 class AddReviewImages < ActiveRecord::Migration
   def change
-    create_table "spree_reviews_images" do |t|
+    create_table "spree_review_images" do |t|
       t.integer "review_id"
       t.boolean "approved", default: false
       t.attachment "attachment"
