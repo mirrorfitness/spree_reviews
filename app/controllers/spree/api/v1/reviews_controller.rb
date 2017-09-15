def4ccbf30fa8ca7d2028d24f69f28410c5ae45d
Asher Van Brunt <asher@okbreathe.com>
2017-09-15 14:50:31 -0700

    Coerce rating to string

--- app/controllers/spree/api/v1/reviews_controller.rb
+++ app/controllers/spree/api/v1/reviews_controller.rb
@@ -13,7 +13,7 @@ module Spree
         def create
           authorize! :create, Review
 
-          params[:review][:rating].sub!(/\s*[^0-9]*\z/, '') unless params[:review][:rating].blank?
+          params[:review][:rating].to_s.sub!(/\s*[^0-9]*\z/, '') unless params[:review][:rating].blank?
 
           @review = Spree::Review.new(review_params)
           @review.product = @product
