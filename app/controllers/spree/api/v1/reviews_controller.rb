2009374a464c8875c6beb791e0fe595220880a9e
Asher Van Brunt <asher@okbreathe.com>
2018-07-31 16:14:03 -0700

    Send nothing back on create if require approval

--- app/controllers/spree/api/v1/reviews_controller.rb
+++ app/controllers/spree/api/v1/reviews_controller.rb
@@ -29,7 +29,11 @@ module Spree
           @review.locale = I18n.locale.to_s if Spree::Reviews::Config[:track_locale]
 
           if @review.save
-            respond_with(@review, status: 201, default_template: :show)
+            if Spree::Reviews::Config[:include_unapproved_reviews]
+              respond_with(@review, status: 201, default_template: :show)
+            else
+              head :no_content
+            end
           else
             invalid_resource!(@review)
           end
