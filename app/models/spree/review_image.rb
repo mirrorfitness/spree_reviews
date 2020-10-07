24a532c5b43e539007aaff90ffad1773d72604a5
Asher Van Brunt <asher@vanbrunt.me>
2020-10-07 15:41:41 -0700

    Switch to activestorage

--- app/models/spree/review_image.rb
+++ app/models/spree/review_image.rb
@@ -1,10 +1,9 @@
 class Spree::ReviewImage < ActiveRecord::Base
-  has_attached_file :attachment, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_one_attached :attachment
+  validates :attachment, file_content_type: { allow: ['image/jpeg', 'image/png'], if: -> { attachment.attached? } }
 
   belongs_to :review
 
   scope :approved, -> { where(approved: true) }
   scope :not_approved, -> { where(approved: false) }
-
-  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
 end
